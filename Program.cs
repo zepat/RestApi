99e41d2b708cf86fe2494a330e2e05867bc8b79e
zepat <patrick.tecles@gmail.com>
2016-09-09 19:10:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+using System.Web;
+using System.Net;
+using System.IO;
+
+namespace ConsoleAppRestHttp
+{
+    public class Class1
+    {
+        //private const string URL = "https://sub.domain.com/objects.json?api_key=123";
+
+
+        private const string URL = "http://jsonplaceholder.typicode.com/";
+
+        private const string DATA = @"{""object"":{""name"":""Name""}}";
+
+        static void Main(string[] args)
+        {
+            Class1.CreateObject();
+        }
+
+        private static void CreateObject()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            request.ContentLength = DATA.Length;
+            using (Stream webStream = request.GetRequestStream())
+            using (StreamWriter requestWriter = new StreamWriter(webStream, System.Text.Encoding.ASCII))
+            {
+                requestWriter.Write(DATA);
+            }
+
+            try
+            {
+                WebResponse webResponse = request.GetResponse();
+                using (Stream webStream = webResponse.GetResponseStream())
+                {
+                    if (webStream != null)
+                    {
+                        using (StreamReader responseReader = new StreamReader(webStream))
+                        {
+                            string response = responseReader.ReadToEnd();
+                            Console.Out.WriteLine(response);
+                            //Console.ReadLine();
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine("-----------------");
+                Console.Out.WriteLine(e.Message);
+            }
+
+        }
+    }
+
+    
+}
+
+
+
+   
